cd533ee4e03ed3a0cf44e73be258535c1b058c11
Rohan Roy <b3you0870@gmail.com>
2017-09-03 03:03:23 +0530

    urls changed

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.views.generic import RedirectView
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,4 +8,9 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='game:login', permanent=True), name='login'),
     url(r'^game/', include('game.urls', namespace='game')),
     url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns += [
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
 ]
