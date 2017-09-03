8b6214e924e3d15c31a8b1c1d7a80b41275eb9c7
Rohan Roy <b3you0870@gmail.com>
2017-09-03 14:02:20 +0530

    updated

--- game/urls.py
+++ game/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^data/$', views.index, name='index'),
+    url(r'^play/$', views.index, name='play'),
     url(r'^(?P<pk>\d+)/$', views.game, name='detail'),
     url(r'^$', views.login, name='login')
 ]
